2b7236a50dc4a80f10aaeeed493b8a95f5379347
OrewaKimi <kimi93.com@gmail.com>
2025-04-18 09:53:05 +0700

    update color, and icon menu options

--- lib/menu_items.dart
+++ lib/menu_items.dart
@@ -7,23 +7,20 @@ class MenuItems extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        buildMenuItem('Menu 1', Icons.dashboard, () {
-          // action menu 1
-        }),
-        buildMenuItem('Menu 2', Icons.settings, () {
-          // action menu 2
-        }),
-        buildMenuItem('Menu 3', Icons.info, () {
-          // action menu 3
-        }),
+        buildMenuItem('Menu 1', Icons.dashboard, () {}),
+        buildMenuItem('Menu 2', Icons.settings, () {}),
+        buildMenuItem('Menu 3', Icons.info, () {}),
       ],
     );
   }
 
   Widget buildMenuItem(String title, IconData icon, VoidCallback onTap) {
     return ListTile(
-      leading: Icon(icon, color: Colors.white),
-      title: Text(title, style: const TextStyle(color: Colors.white)),
+      leading: Icon(icon, color: Colors.deepPurple),
+      title: Text(
+        title,
+        style: const TextStyle(color: Colors.black87),
+      ),
       onTap: onTap,
     );
   }
